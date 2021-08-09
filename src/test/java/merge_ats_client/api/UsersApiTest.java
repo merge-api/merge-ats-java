4303e813f127c75af3fb1a9728666f0bbd46003a
Dan <dan@merge.dev>
2021-08-09 11:40:16 -0700

    fix test

--- src/test/java/merge_ats_client/api/UsersApiTest.java
+++ src/test/java/merge_ats_client/api/UsersApiTest.java
@@ -49,12 +49,13 @@ public class UsersApiTest {
         OffsetDateTime createdAfter = null;
         OffsetDateTime createdBefore = null;
         String cursor = null;
+        String email = null;
         Boolean includeRemoteData = null;
         OffsetDateTime modifiedAfter = null;
         OffsetDateTime modifiedBefore = null;
         Integer pageSize = null;
         String remoteId = null;
-        PaginatedRemoteUserList response = api.usersList(xAccountToken, createdAfter, createdBefore, cursor, includeRemoteData, modifiedAfter, modifiedBefore, pageSize, remoteId);
+        PaginatedRemoteUserList response = api.usersList(xAccountToken, createdAfter, createdBefore, cursor, email, includeRemoteData, modifiedAfter, modifiedBefore, pageSize, remoteId);
 
         // TODO: test validations
     }
